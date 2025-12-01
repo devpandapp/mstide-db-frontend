f9e74bf8d8276ba30acd14f4197e333e458b2b6b
devpandapp <dev.pandapp@gmail.com>
2025-12-02 00:34:32 +0100

    feat: added footer for About page

---
+++ src/components/organisms/OrganismFooter.vue
@@ -0,0 +1,7 @@
+<template>
+  <v-footer app height="50">
+    <div class="d-flex ms-auto">
+      Developed by&nbsp;<a href="https://www.linkedin.com/in/aapand" target="_blank">Pandapp</a>
+    </div>
+  </v-footer>
+</template>
--- src/components/templates/TemplateAbout.vue
+++ src/components/templates/TemplateAbout.vue
@@ -2,11 +2,13 @@
 // Components
 import OrganismHeader from '@/components/organisms/OrganismHeader.vue';
 import OrganismAbout from '@/components/organisms/OrganismAbout.vue';
+import OrganismFooter from '@/components/organisms/OrganismFooter.vue';
 </script>
 
 <template>
   <v-layout>
     <OrganismHeader />
     <OrganismAbout />
+    <OrganismFooter />
   </v-layout>
 </template>
